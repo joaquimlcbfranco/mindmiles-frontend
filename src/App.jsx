c724cfa30cbe7350145d7d9441bd61ee5b73d8b2
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-22 16:07:38 +0100

    feat: Add footer to App component for testing purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import style from "./App.module.css";
 import Header from "./components/header/Header.jsx";
 import Activity from "./components/activity/Activity.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
 	return (
 		<>
-				<Header />
-        <div className={style.content}>
-          <Activity />
-          <Activity />
-          <Activity />
-          <Activity />
-          <Activity />
-          <Activity />
-          
-        </div>
+			<Header />
+			<div className={style.content}>
+				<Activity />
+				<Activity />
+				<Activity />
+				<Activity />
+				<Activity />
+				<Activity />
+			</div>
+			<Footer />
 		</>
 	);
 }
