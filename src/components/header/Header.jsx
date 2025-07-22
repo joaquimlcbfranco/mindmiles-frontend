754fff485f2b9f0662a669a16d98dd8fe3d75199
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-22 12:47:28 +0100

    feat: Add svg instead of words to header, add MindMiles heading as home button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,47 @@ import style from "./Header.module.css";
 
 export default function Header() {
 	return (
-		<header className={style.header}>
-			<div>
-				<button>Menu</button>
-			</div>
-			<div className={style.rightHeader}>
-				<button>Timer</button>
-				<button>Profile</button>
-			</div>
-		</header>
+		<div className={style.headerWrapper}>
+			<header className={style.header}>
+				<div className={style.leftHeader}>
+					<h1><span>M</span>ind<span>M</span>iles</h1>
+					<button>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							height="24px"
+							viewBox="0 -960 960 960"
+							width="24px"
+							fill="white"
+						>
+							<path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+						</svg>
+					</button>
+				</div>
+				<div className={style.rightHeader}>
+					<button>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							height="24px"
+							viewBox="0 -960 960 960"
+							width="24px"
+							fill="white"
+						>
+							<path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z" />
+						</svg>
+					</button>
+					<button>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							height="24px"
+							viewBox="0 -960 960 960"
+							width="24px"
+							fill="white"
+						>
+							<path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
+						</svg>
+					</button>
+				</div>
+			</header>
+		</div>
 	);
 }
