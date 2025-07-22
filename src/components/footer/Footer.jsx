14b44aa752c08aad603f0ec12193b2e705e6bcfa
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-22 16:07:16 +0100

    feat: Create and style simple footer sections, links to linkedin profile

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import style from "./Footer.module.css";
+
+export default function Footer() {
+	return (
+		<div className={style.footerWrapper}>
+			<p><a href="https://www.linkedin.com/in/joaquimlcbfranco/">joaquimlcbfranco</a> | Copyright &copy; 2025</p>
+		</div>
+	);
+}
