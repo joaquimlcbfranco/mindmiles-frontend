ff05e80654e14bd8ab108a9397740924e51c076b
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-22 17:05:41 +0100

    feat: Remove individual card components, add in Feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import style from "./App.module.css";
-import Header from "./components/header/Header.jsx";
-import Activity from "./components/activity/Activity.jsx";
-import Footer from "./components/footer/Footer.jsx";
+import Feed from "./pages/feed/Feed.jsx";
 
 function App() {
 	return (
 		<>
-			<Header />
-			<div className={style.content}>
-				<Activity />
-				<Activity />
-				<Activity />
-				<Activity />
-				<Activity />
-				<Activity />
-			</div>
-			<Footer />
+			<Feed />
 		</>
 	);
 }
