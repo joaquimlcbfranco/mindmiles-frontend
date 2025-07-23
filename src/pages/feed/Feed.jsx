cdcc03b4cd0e1d0e74b92ceae98eb0a1b2de3fb8
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-23 16:05:15 +0100

    feat: Add basic auth to test connection to backend

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -3,11 +3,44 @@ import style from "./Feed.module.css";
 import Header from "../../components/header/Header.jsx";
 import Activity from "../../components/activity/Activity.jsx";
 import Footer from "../../components/footer/Footer.jsx";
+import { useEffect, useState } from "react";
 
 export default function Feed() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const url = "http://localhost:8080/api/users";
+		async function getPosts() {
+			try {
+				const username = "jaknito";
+				const password = "test1234";
+				const basicAuth = btoa(`${username}:${password}`);
+
+				const response = await fetch(url, {
+					method: "GET",
+					headers: {
+						Authorization: `Basic ${basicAuth}`,
+					},
+				});
+
+				if (!response.ok) {
+					console.log("Response status: " + response.status);
+				}
+
+				const json = await response.json();
+				setPosts(json);
+			} catch (error) {
+				console.log(error.message);
+			}
+		}
+
+		getPosts();
+	}, []);
+
+	console.log(posts);
+
 	return (
 		<>
-			<Header />
 			<div className={style.content}>
 				<Activity />
 				<Activity />
@@ -16,7 +49,6 @@ export default function Feed() {
 				<Activity />
 				<Activity />
 			</div>
-			<Footer />
 		</>
 	);
 }
