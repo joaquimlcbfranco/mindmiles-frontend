16458f45bef46f65c50ceb0775cc1eb0d071a874
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-25 13:39:36 +0100

    feat: Make server error page receive an optional detailed error message

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,10 +1,10 @@
 import style from "./ErrorPage.module.css";
 import { Link } from "react-router-dom";
 
-export default function ErrorPage() {
+export default function ErrorPage({ message = "" }) {
 	return (
 		<div className={style.errorWrapper}>
-			<h1>Sorry! There was an error handling your request</h1>
+			<h1>Sorry! There was an error handling your request - ({message})</h1>
       <p>Back to <Link to="/">homepage</Link></p>
 		</div>
 	);
